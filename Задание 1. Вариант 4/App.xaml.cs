c2ccdb63c69ff7fa4d17287d02ff7cb93ee0811c
pc202020 <pc202020@pc202020.ru>
2023-11-24 08:35:05 +0300

    Добавьте файлы проекта.

---
+++ Задание 1. Вариант 4/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Задание_1._Вариант_4
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
